161936529f6435707080bee1536d64d3addf51ca
Dan M <daniel@thewindmills.com.au>
2023-06-20 16:19:42 +1000

    Implemented a much better meshing algorithim

--- addons/kvox_importer/voxel_mesh.gd
+++ addons/kvox_importer/voxel_mesh.gd
@@ -17,20 +17,26 @@ const _points = [
 	Vector3(1,1,1), #7
 ]
 
-const _tris = [
-	Vector3(2,1,0),
-	Vector3(1,2,3),
-	Vector3(0,1,4),
-	Vector3(5,4,1),
-	Vector3(4,6,2),
-	Vector3(0,4,2),
-	Vector3(5,7,6),
-	Vector3(4,5,6),
-	Vector3(5,1,3),
-	Vector3(3,7,5),
-	Vector3(6,7,3),
-	Vector3(3,2,6)
-]
+enum Faces {X, NEG_X, Y, NEG_Y, Z, NEG_Z}
+
+
+const _dirs = {
+	Faces.X:Vector3(1,0,0),
+	Faces.Y:Vector3(0,1,0),
+	Faces.Z:Vector3(0,0,1),
+	Faces.NEG_X:Vector3(-1,0,0),
+	Faces.NEG_Y:Vector3(0,-1,0),
+	Faces.NEG_Z:Vector3(0,0,-1),
+}
+
+const _verts = {
+	Faces.X:[Vector3(1,0,0), Vector3(1,0,1), Vector3(1,1,0), Vector3(1,1,1)],
+	Faces.NEG_X:[Vector3(0,1,1), Vector3(0,0,1),Vector3(0,1,0), Vector3(0,0,0)],
+	Faces.Y:[Vector3(0,1,0), Vector3(1,1,0), Vector3(0,1,1), Vector3(1,1,1)],
+	Faces.NEG_Y:[Vector3(1,0,1), Vector3(1,0,0), Vector3(0,0,1), Vector3(0,0,0)],
+	Faces.Z:[Vector3(0,0,1), Vector3(0,1,1), Vector3(1,0,1), Vector3(1,1,1)],
+	Faces.NEG_Z:[Vector3(1,1,0), Vector3(0,1,0), Vector3(1,0,0), Vector3(0,0,0)],
+}
 
 func _init(_size: Vector3i = Vector3i(1,1,1), data = [[[1]]]):
 	mesh_data = data
@@ -38,9 +44,6 @@ func _init(_size: Vector3i = Vector3i(1,1,1), data = [[[1]]]):
 	remesh()
 
 func remesh():
-	var mapping_dict = {
-		
-	}
 	var tris = []
 	var surface_array = []
 	surface_array.resize(Mesh.ARRAY_MAX)
@@ -48,49 +51,52 @@ func remesh():
 	var uvs = PackedVector2Array()
 	var normals = PackedVector3Array()
 	var indices = PackedInt32Array()
-	
-	var point = 0
+	var count = 0
 	
 	for x in range(size.x):
 		for y in range(size.y):
 			for z in range(size.z):
 				if (mesh_data[x][y][z] != 0):
-					var i: float = 0
-					for p in _points:
-						var vert: Vector3 = p+Vector3(x,y,z)
-						var index = verts.find(vert)
-						if (index == -1):
-							verts.append(vert)
-							normals.append(vert.normalized())
-							index = verts.size()-1
-						mapping_dict[point+i] = index
-						i+=1
-					for tri in _tris:
-						var p1 = mapping_dict[point+tri.x]
-						var p2 = mapping_dict[point+tri.y]
-						var p3 = mapping_dict[point+tri.z]
-						var t =   Vector3(p1, p2, p3)
-						var ts = [
-							t,
-							Vector3(p2, p1, p3),
-							Vector3(p1, p3, p2), 
-							Vector3(p2, p3, p1), 
-							Vector3(p3, p1, p2),
-							Vector3(p3,p2, p1),
-							]
-						var include = true
-						for _t in ts:
-							if (tris.find(_t) != -1):
-								include = false
-								break
-						if (include):
-							tris.append(t)
-							
-					point += _points.size()
+					count += 1
+					for k in _dirs.keys():
+						var dir = _dirs[k]
+						var x_off = x+dir.x
+						var y_off = y+dir.y
+						var z_off = z+dir.z
+						var empty_space = false
+						if (
+							x_off < 0 || 
+							x_off >= size.x ||
+							y_off < 0 ||
+							y_off >= size.y ||
+							z_off < 0 ||
+							z_off >= size.z
+						):
+							empty_space = true
+						else:
+							empty_space = mesh_data[x_off][y_off][z_off] == 0
+						if (empty_space):
+							var i = verts.size()
+							for v in _verts[k]:
+								var vert: Vector3 = v+Vector3(x,y,z)
+								verts.append(vert)
+								normals.append(vert.normalized())
+							indices.append_array([
+								i+0,
+								i+1,
+								i+2,
+								i+3,
+								i+2,
+								i+1
+							])
+						
 	for tri in tris:
 		indices.append_array([tri.x, tri.y, tri.z])
-	print(verts.size())
-	print(indices.size()/3)
+	print("=====")
+	print("For %s voxels" % count)
+	print("%s\tVertices" % verts.size())
+	print("%s\tTris" % (indices.size()/3))
+	print("%s\tFaces" % (indices.size()/6))
 	
 	surface_array[Mesh.ARRAY_VERTEX] = verts
 	surface_array[Mesh.ARRAY_NORMAL] = normals
