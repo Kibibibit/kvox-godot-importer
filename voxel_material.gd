6d7b233daf0a2061d6cfa69dbb9b4e463786bdad
Kibibibit <daniel@thewindmills.com.au>
2023-06-24 22:54:53 +1000

    Changes to help with editor

--- voxel_material.gd
+++ voxel_material.gd
@@ -7,15 +7,15 @@ var color: Color
 var metallic: int
 @export_range(0,255)
 var roughness: int
-@export
-var emission_energy: float
+@export_range(0,255)
+var emission_energy: int
 
 ## Takes in raw bytes for each field and converts them to the relevant datatypes
 func create_from_bytes(r:int, g:int, b:int, metal:int, rough:int, emission:int) -> void:
 	color = Color8(r,g,b)
 	metallic = metal
 	roughness = rough
-	emission_energy = emission/255.0
+	emission_energy = emission
 
 func metal_color() -> Color:
 	return Color8(metallic,0,0)
@@ -29,11 +29,12 @@ func to_standard_material():
 	out.metallic = metallic/255.0
 	out.roughness = roughness/255.0
 	out.emission_enabled = true
-	out.emission_enabled = 3.33
+	out.emission_energy_multiplier = 3.33
+	var e: float = emission_energy/255.0
 	out.emission = Color(
-			color.r*emission_energy,
-			color.g*emission_energy,
-			color.b*emission_energy
+			color.r*e,
+			color.g*e,
+			color.b*e
 		)
 	return out
 
